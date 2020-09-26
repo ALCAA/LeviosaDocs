7cd9b17666bab37693822acb8b58c0de9989fd55
Elodie LEGER <elodiesj.leger@yahoo.fr>
2020-09-26 23:07:57 +0200

    [SOCKET] fixed bug with asynchronous calls, now works

--- leviosaeditor/server.js
+++ leviosaeditor/server.js
@@ -11,7 +11,6 @@ http.listen(port, () => log(`server listening on port: ${port}`));
 io.on('connection', (socket) => {
     log('connected');
     socket.on('message', (evt) => {
-        //log(evt);
         socket.broadcast.emit('message', evt);
     });
 })
--- leviosaeditor/src/pages/Editor.js
+++ leviosaeditor/src/pages/Editor.js
@@ -17,9 +17,11 @@ class Editor extends Component {
 
 	//after event of typing in textbox, set text with the new value
 	//and send it by socket message, found in server.js
+
+	//asynchronous so can't send this.state.text (or there is a character missing)
 	sendToSocket = (event) => {
 		this.setState({text : event.target.value});
-		socket.send(this.state.text);
+		socket.send(event.target.value);
 	}
 
 	//Listen on messages incoming from socket and set text with data from Socket
@@ -28,8 +30,7 @@ class Editor extends Component {
 			this.setState({text : data});
 		})
 	}
-
-
+	
 	render = () => {
 		return (
 			<div>
