924b8e0f2437bf8206073214f3aaf86d27e1ae76
Elodie LEGER <elodiesj.leger@yahoo.fr>
2020-09-26 22:30:32 +0200

    [SOCKET] add socket io just needs to get last char

---
+++ leviosaeditor/server.js
@@ -0,0 +1,20 @@
+const log = console.log;
+//initialize http server, socket.io and port number
+const http = require('http').createServer();
+const io = require('socket.io')(http);
+const port = 8000;
+http.listen(port, () => log(`server listening on port: ${port}`));
+
+//Listen for incoming connection event
+//then listen for incoming message on recepted messages
+//and broadcast message, found in setTextFromSocket in Editor.js
+io.on('connection', (socket) => {
+    log('connected');
+    socket.on('message', (evt) => {
+        //log(evt);
+        socket.broadcast.emit('message', evt);
+    });
+})
+io.on('disconnect', (evt) => {
+    log('some people left');
+})
--- leviosaeditor/src/pages/Editor.js
+++ leviosaeditor/src/pages/Editor.js
@@ -1,17 +1,42 @@
 import React, { Component } from 'react';
 import './Editor.css';
+import socketIOClient from "socket.io-client";
+
+//construct socket
+const socket = socketIOClient('http://localhost:8000');
 
 class Editor extends Component {
+	constructor(){
+		super();
 
-	render() {
+		this.state = {
+			text : ''
+		}
+		this.setTextFromSocket();
+	}
+
+	//after event of typing in textbox, set text with the new value
+	//and send it by socket message, found in server.js
+	sendToSocket = (event) => {
+		this.setState({text : event.target.value});
+		socket.send(this.state.text);
+	}
+
+	//Listen on messages incoming from socket and set text with data from Socket
+	setTextFromSocket = () => {
+		socket.on('message', (data) => {
+			this.setState({text : data});
+		})
+	}
+
+
+	render = () => {
 		return (
 			<div>
     			<h3>Realtime Editor/Collaboration</h3>
     			<div>
-        			<textarea id='editor' placeholder="Type Your Text..."></textarea>
+        			<textarea onChange={this.sendToSocket} value={this.state.text} id='editor' placeholder="Type Your Text..."></textarea>
     			</div>
-			<script src="../../node_modules/socket.io-client/dist/socket.io.js"></script>
-			<script src="../client.js"></script>
 			</div>
 		)
 	}
