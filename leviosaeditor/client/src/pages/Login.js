57c70fad06cfc4c8165cb75ed2ad51cbddcf10c9
ArthurBiragnet <arthur.biragnet@epita.fr>
2020-09-30 02:16:55 +0200

    [PAGES] Add Login and Documents pages

---
+++ leviosaeditor/client/src/pages/Login.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from "@material-ui/core/Button";
+import { Link } from 'react-router-dom';
+import '../App.css'
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        '& > *': {
+            margin: theme.spacing(1),
+            width: '25ch',
+        },
+    },
+    margin: {
+        margin: theme.spacing(1),
+    },
+}));
+
+
+
+class Login extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            username: ''
+        }
+    }
+
+    render () {
+        const { classes } = this.props;
+
+        return (
+            <div className='Login'>
+                <header className='App-header'/>
+                <body className='App-body'>
+                    <form className={classes.root} autoComplete="off" noValidate>
+                        <div className="login-items">
+                            <TextField id="outlined-basic"
+                                       label="Username"
+                                       variant="outlined"
+                                       value={this.state.username}
+                                       onChange={(e) => {this.setState({username: e.target.value});}}
+                                       required />
+                            <Link to="/documents">
+                                <Button id='submit-login' variant="contained" color="primary" className={classes.submit}>
+                                    Continuer
+                                </Button>
+                            </Link>
+                        </div>
+                    </form>
+                </body>
+            </div>
+        )
+    }
+}
+
+export default withStyles(useStyles)(Login)
