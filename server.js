abd3ceb7090159ae34b37edc00ae753fefe05f84
ALCAA <antoine.trollet@outlook.com>
2020-10-04 22:30:55 +0200

    [BACKEND] users creation and connection with validators

--- server.js
+++ server.js
@@ -1,9 +1,53 @@
-const log = console.log;
-//initialize http server, socket.io and port number
-const http = require('http').createServer();
-const io = require('socket.io')(http);
-const port = 8000;
-http.listen(port, () => log(`server listening on port: ${port}`));
+/**
+ *
+ */
+
+// Load all the libraries for the server
+const express = require('express')
+const http = require('http')
+const socketIo = require('socket.io')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const passport = require('passport')
+const cookieParser = require('cookie-parser')
+
+// Routes
+const users = require('./routes/users')
+
+// Setup server
+const port = process.env.PORT || 8000
+const app = express()
+const server = http.createServer(app)
+const io = socketIo(server)
+
+// Bodyparser middleware
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+)
+app.use(bodyParser.json())
+app.use(cookieParser())
+
+// DB Config
+const db = require('./config/keys').mongoURI
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log('MongoDB successfully connected'))
+  .catch(err => console.log(err))
+
+// Passport middleware
+app.use(passport.initialize())
+
+// Passport config
+require('./config/passport')(passport)
+
+app.use('/users', users)
 
 //Listen for incoming connection event
 //then listen for incoming message on recepted messages
@@ -17,3 +61,5 @@ io.on('connection', (socket) => {
 io.on('disconnect', (evt) => {
     console.log('some people left');
 })
+
+server.listen(port, () => console.log(`server listening on port: ${port}`));
